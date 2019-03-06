fa93237795d83e4bb8b32bf14f414ff28a633b5f
Bitsgap <30630956+Bitsgap@users.noreply.github.com>
2019-03-06 11:52:33 +0300

    Update rest-api-php.php

--- rest-api-php.php
+++ rest-api-php.php
@@ -28,10 +28,12 @@ class RESTbitsgap
       return $sendData;
     }
 
+    /* Signature message content with private key */
     protected function getSign($data_str) {
 		    return hash_hmac('SHA512', $data_str, $this->privateKey);
     }
 
+    /* Send HTTP POST Request */
     protected function httpRequest($url, $data = null){
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
@@ -45,6 +47,7 @@ class RESTbitsgap
         return $output;
     }
 
+    /* Prepare data and send request */
     public function request($name, $params=null) {
 	     $sendData  = $this->getData($params);
        $url = $this->url.'/'.$name;
@@ -148,14 +151,19 @@ class RESTbitsgap
 
     // Trade methods
 
+    /* Get list of all user API keys for real markets and key status */
+    public function api_keys() {
+      return $this->request('keys');
+    }
+
     /*
-     * Get balances
+     * Get balances for all markets
+     * Returns balance info, checked
      */
     public function balances($is_demo=false) {
       $request_path = 'balance';
-      if($is_demo) {
+      if($is_demo)
         $request_path = 'demo/'.$request_path;
-      }
       return $this->request($request_path);
     }
 
@@ -169,9 +177,10 @@ class RESTbitsgap
       $params = [
         'market'  => $market
       ];
-      if($is_demo) {
+
+      if($is_demo)
         $request_path = 'demo/'.$request_path;
-      }
+
       return $this->request($request_path, $params);
     }
 
@@ -185,9 +194,9 @@ class RESTbitsgap
         'market'  => $market
       ];
       $request_path = 'orders/history';
-      if($is_demo) {
+      if($is_demo)
         $request_path = 'demo/'.$request_path;
-      }
+
       return $this->request($request_path, $params);
     }
 
@@ -210,14 +219,13 @@ class RESTbitsgap
         'type'    => $type,
       ];
 
-      if (isset($price)) {
+      if (isset($price))
         $params['price'] = $price;
-      }
 
       $request_path = 'orders/add';
-      if($is_demo) {
+      if($is_demo)
         $request_path = 'demo/'.$request_path;
-      }
+
       return $this->request($request_path, $params);
     }
 
@@ -227,15 +235,16 @@ class RESTbitsgap
      *   market - market name
      *   pid - market order id
      */
-    public function order_cancel($market, $pid, $is_demo=false) {
+    public function order_cancel($market, $id, $is_demo=false) {
       $params = [
         'market'  => $market,
-        'pid'     => $pid
+        'id'     => $id
       ];
       $request_path = 'orders/cancel';
-      if($is_demo) {
+
+      if($is_demo)
         $request_path = 'demo/'.$request_path;
-      }
+
       return $this->request($request_path, $params);
     }
 }
@@ -246,50 +255,52 @@ $rest_api = new RESTbitsgap();
 /* Market methods */
 
 // test markets
-print_r($rest_api->markets());
+// print_r($rest_api->markets());
 
 // test last-price
-//print_r($rest_api->last_price('kraken', 'BTC_USD'));
+print_r($rest_api->last_price('kraken', 'BTC_USD'));
 
 // test market and pair orderbook
-//print_r($rest_api->orderbook('kraken', 'BTC_USD'));
+// print_r($rest_api->orderbook('kraken', 'BTC_USD'));
 
 // test market and pair ticker orderbook in selected interval
-//print_r($rest_api-> tick_orderbook('kraken', 'BTC_USD', time() - 3600, time()));
+// print_r($rest_api-> tick_orderbook('kraken', 'BTC_USD', time() - 3600, time()));
 
 // test market and pair ticker transactions in selected interval
-//print_r($rest_api->tick_transactions('kraken', 'BTC_USD', time() - 3600, time()));
+// print_r($rest_api->tick_transactions('kraken', 'BTC_USD', time() - 3600, time()));
 
 // test market and pair OHLC in selected interval
-//print_r($rest_api->ohlc('kraken', 'BTC_USD', time() - 3600, time()));
+// print_r($rest_api->ohlc('kraken', 'BTC_USD', time() - 3600, time()));
 
 // test market and pair Recent Trades
-//print_r($rest_api->recent_trades('kraken', 'BTC_USD'));
+// print_r($rest_api->recent_trades('kraken', 'BTC_USD'));
 
 /* User methods */
 
+// user api keys
+// print_r($rest_api->api_keys());
+
 // user balance real markets
-print_r($rest_api->balances());
+// print_r($rest_api->balances());
 // user balance demo markets
-print_r($rest_api->balances(true));
+// print_r($rest_api->balances(true));
 
 // user open orders real markets
-//print_r($rest_api->orders_open('idex'));
+// print_r($rest_api->orders_open('bit-z'));
 // user open orders demo markets
-//print_r($rest_api->orders_open('kraken', true));
+// print_r($rest_api->orders_open('kraken', true));
 
 // user closed orders real markets
-//print_r($rest_api->orders_history('kraken'));
+// print_r($rest_api->orders_history('kraken'));
 // user closed orders demo markets
-//print_r($rest_api->orders_history('kraken', true));
+// print_r($rest_api->orders_history('kraken', true));
 
 // user place order real markets
-//print_r($rest_api->order_place('bittrex', 'EDR_USD', '150', 'sell', 'limit', '0.2'));
+// print_r($rest_api->order_place('bit-z', 'ETH_BTC', '0.001', 'buy', 'limit', '0.030'));
 // user place order demo markets
-//print_r($rest_api->order_place('kraken', 'ADA_USD', '10', 'buy', 'limit', '0.04', true));
-//print_r($rest_api->order_place('kraken', 'ADA_USD', '10', 'buy', 'market', null, true));
+// print_r($rest_api->order_place('kraken', 'ADA_USD', '10', 'buy', 'limit', '0.02', true));
 
 // user cancel order real market
-//print_r($rest_api->order_cancel('bit-z', '16xx68xx1x1'));
+// print_r($rest_api->order_cancel('bit-z', '1691634186'));
 // user cancel order demo market
-//print_r($rest_api->order_cancel('kraken', '5fxxxxxxxxxxxxxxxxxxxxxxxxxx32', true));
+// print_r($rest_api->order_cancel('kraken', '4d342eef9cf74c72b3d08a897a7259c1', true));
